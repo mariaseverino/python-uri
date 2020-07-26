748ac12c816abd6d96b02d643cf89bd82ab8945b
Maria Severino <mariarsseverino@gmail.com>
2020-07-26 17:41:17 -0300

    ✨ add 1008

---
+++ 1008.py
@@ -0,0 +1,7 @@
+numero = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+
+salario = horasTrabalhadas * valorHora
+
+print("NUMBER = {}\nSALARY = U$ {:.2f}".format(numero, salario))
